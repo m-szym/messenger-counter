1a02568f9a4b2cf36468fad18503912c2eb319b5
Marek Szymański <115031525+m-szym@users.noreply.github.com>
2024-08-17 18:49:30 +0200

    Update counter.py

--- counter.py
+++ counter.py
@@ -7,35 +7,13 @@ import json
 import re
 import datetime
 
-
-# Some pre-made sorting methods
-# Method can be 'name', 'messages_nr', 'participants_nr', 'first', 'last', 'duration'
-def sort_stats(_stats, method):
-    if method == 'name':
-        _sorted_stats = sorted(_stats.items(), key=lambda x: x[0])
-    elif method == 'messages_nr':
-        _sorted_stats = sorted(_stats.items(), key=lambda x: x[1]['messages_nr'], reverse=True)
-    elif method == 'participants_nr':
-        _sorted_stats = sorted(_stats.items(), key=lambda x: len(x[1]['participants']), reverse=True)
-    elif method == 'first':
-        _sorted_stats = sorted(_stats.items(), key=lambda x: x[1]['first'], reverse=True)
-    elif method == 'last':
-        _sorted_stats = sorted(_stats.items(), key=lambda x: x[1]['last'], reverse=True)
-    elif method == 'duration':
-        _sorted_stats = sorted(_stats.items(), key=lambda x: x[1]['last'] - x[1]['first'], reverse=True)
-    else:
-        print('Invalid method')
-        return _stats
-    return _sorted_stats
-
-
 if __name__ == '__main__':
     inbox_path = 'F:\\your_facebook_activity\\messages\\inbox'  # <--- CHANGE THIS
     # Path to the folder containing the messages - default is something like "your_facebook_activity\messages\inbox"
     # Replace \ with \\ or / (because of escape characters)
 
     stats = {}
-    # Dictionary to store the info for each conversation
+    # Dictionary to store the info for each chat
     # the format is:
     # {
     #     "name": {
@@ -45,7 +23,7 @@ if __name__ == '__main__':
     #         "last": timestamp of the last message
     #     }
     # }
-    # * encrypted conversations don't have the participants list
+    # * encrypted chats don't have the participants list
     # ** timestamps are Unix time in milliseconds
 
     for folder in os.listdir(inbox_path):
@@ -75,8 +53,8 @@ if __name__ == '__main__':
                 else:
                     first = 0
 
-        conv_name = re.sub(r'_[0-9]+', ' ', folder).strip()
-        stats[conv_name] = {
+        chat_name = re.sub(r'_[0-9]+', ' ', folder).strip()
+        stats[chat_name] = {
             "messages_nr": messages_count,
             "participants": participants,
             "first": first,
@@ -84,20 +62,32 @@ if __name__ == '__main__':
         }
 
     # Some useful filters
-    # stats = {k: v for k, v in stats.items() if len(v["participants"]) <= 2}   # No group chats
-    # stats = {k: v for k, v in stats.items() if v["messages_nr"] >= 10}        # No less than 10 messages
+    # stats = {k: v for k, v in stats.items() if len(v["participants"]) <= 2}                 # No group chats
+    # stats = {k: v for k, v in stats.items() if v["messages_nr"] >= 10}                      # No less than 10 messages
 
-    # Sort the stats by the number of messages
-    stats_sorted = sort_stats(stats, 'messages_nr')
+    # Some sorting methods
+    stats_sorted = sorted(stats.items(), key=lambda x: x[0])                                # By chat name
+    # stats_sorted = sorted(stats.items(), key=lambda x: x[1]['messages_nr'], reverse=True)   # By number of messages
+    # stats_sorted = sorted(stats.items(), key=lambda x: len(x[1]['participants']),
+    #                       reverse=True)                                                     # By nr of participants
+    # stats_sorted = sorted(stats.items(), key=lambda x: x[1]['first'],
+    #                       reverse=True)                                                     # By first timestamp
+    # stats_sorted = sorted(stats.items(), key=lambda x: x[1]['last'],
+    #                       reverse=True)                                                     # By last timestamp
+    # stats_sorted = sorted(stats.items(), key=lambda x: x[1]['last'] - x[1]['first'],
+    #                       reverse=True)                                                     # By chat duration
 
     # Example output, customize as needed
-    for conv_name, info in stats_sorted:
-        name = f'{conv_name if len(conv_name) <= 20 else conv_name[:20] + "..."}'
+    i = 0
+    print("{:<4}  {:<25}  {:<6}  {:<25}  {:<10}  {:<10}".format('#', "Chat name", "Count", "Timespan", "Avg", "Type"))
+    print("-" * 88)
+    for chat_name, info in stats_sorted:
+        name = f'{chat_name if len(chat_name) <= 20 else chat_name[:20] + "..."}'
         basic_info = f'{info["messages_nr"]}'
-        if len(info["participants"]) > 2:
-            basic_info += " [Group]"
         timespan = f'({datetime.datetime.fromtimestamp(info["first"] / 1000).strftime("%Y-%m-%d")} - ' \
                    f'{datetime.datetime.fromtimestamp(info["last"] / 1000).strftime("%Y-%m-%d")})'
-        average = f' Avg: {info["messages_nr"] / ((info["last"] - info["first"]) // 86400000):.2f} /day' \
-                  if (info["last"] - info["first"]) // 86400000 else ''
-        print("{:<25} {:<15} {:<20} {:<10}".format(name, basic_info, timespan, average))
+        average = f'{info["messages_nr"] / ((info["last"] - info["first"]) // 86400000):.2f} /day' \
+            if (info["last"] - info["first"]) // 86400000 else ''
+        group = 'Group' if len(info["participants"]) > 2 else 'Private'
+        print("{:<4}  {:<25}  {:<6}  {:<15}  {:<10}  {:<10}".format(i, name, basic_info, timespan, average, group))
+        i += 1
